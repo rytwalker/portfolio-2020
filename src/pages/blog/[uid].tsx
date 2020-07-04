081c8b992007e8d22ea536f4d5d010c44ae26eec
Ryan Walker <rytwalker@gmail.com>
2020-07-04 00:17:58 -0400

    prismic integration added

---
+++ src/pages/blog/[uid].tsx
@@ -0,0 +1,67 @@
+import Head from "next/head"
+import { RichText } from "prismic-reactjs"
+import { queryRepeatableDocuments } from "../../utils/queries"
+import { Client } from "../../utils/prismic-configuration"
+import { totalmem } from "os"
+
+interface previewData {
+  ref?: any
+}
+
+function BlogPost({ post }) {
+  if (post && post.data) {
+    console.log(post.data)
+    // const hasSeoTitle = RichText.asText(post.data.seo_title).length !== 0
+    const seoTitle = post.data.seo_title || "BLOG"
+    const seoDescription = post.data.seo_description || "Default Description"
+    const hasTitle = RichText.asText(post.data.title).length !== 0
+    const title = hasTitle
+      ? RichText.asText(post.data.title)
+      : "Generic Blog Title Name"
+
+    return (
+      <>
+        <Head>
+          <title>{seoTitle}</title>
+          <meta name="description" content={seoDescription} />
+        </Head>
+        <div>
+          <h2>{title}</h2>
+        </div>
+      </>
+    )
+  }
+
+  return null
+}
+
+export async function getStaticProps({
+  params,
+  preview = null,
+  previewData: previewData = {}
+}) {
+  const { ref }: any = previewData
+  const post = await Client().getByUID(
+    "blog_post",
+    params.uid,
+    ref ? { ref } : null || {}
+  )
+  return {
+    props: {
+      preview,
+      post
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const documents = await queryRepeatableDocuments(
+    (doc) => doc.type === "blog_post"
+  )
+  return {
+    paths: documents.map((doc) => `/blog/${doc.uid}`),
+    fallback: true
+  }
+}
+
+export default BlogPost
