4c827b6f39b1345bf3937ca563a24c750c9b81c9
Ryan Walker <rytwalker@gmail.com>
2020-07-09 09:54:42 -0400

    created pages

---
+++ src/components/Layout/PageLayout.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+import Nav from "../Nav/Nav"
+
+function PageLayout({ children }) {
+  return (
+    <Container>
+      <Nav />
+      {children}
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 144px 0 0;
+`
+
+export default PageLayout
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,21 @@
+// import styled from "styled-components"
+
+import PageLayout from "../../components/Layout/PageLayout"
+
+function Blog() {
+  return (
+    <PageLayout>
+      <h1>Blog</h1>
+      <p>
+        My name is Ryan Walker and I am a full-stack developer from Youngstown,
+        Ohio. Learning how to code started out as a hobby for me, but I grew
+        obsessed with building things that required both creativity and logic.
+        Right now I am currently enrolled at Lambda School where I spend 40+
+        hours a week learning the latest web development trends and fundamental
+        computer science skills to bring my projects to life.
+      </p>
+    </PageLayout>
+  )
+}
+
+export default Blog
